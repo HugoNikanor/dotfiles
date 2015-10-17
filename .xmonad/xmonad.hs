2fbb1ec2b7561e21945825b171c359512117cd02
Hugo <hugo.hornquist@gmail.com>
2015-10-17 22:19:20 +0200

    xmonad now works

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -1,8 +1,35 @@
 import XMonad
+import Data.Monoid
+import System.Exit
 
-main = xmonad defaultConfig
-         { terminal = "gnome-terminal" 
-		 , modMask = mod4Mask
-         , terminal = "urxvt"
-         }
+import qualified XMonad.StackSet as W
+import qualified Data.Map        as M
+
+
+myTerminal = "gnome-terminal"
+
+myFocusFollowsMouse :: Bool
+myFocusFollowsMouse = False
+
+myModMask = mod4Mask
+
+
+myKeys conf@(XConfig {XMonad.modMask = modm}) = M.fromList $
+    [ ((modm , xK_w) , sendMessage (IncMasterN 1))
+    , ((modm , xK_v) , sendMessage (IncMasterN (-1))) 
+    ]
+    ++
+
+    [((m .|. modm, key), screenWorkspace sc >>= flip whenJust (windows . f))
+        | (key, sc) <- zip [xK_a, xK_o, xK_e, xK_u] [0..]
+        , (f, m) <- [(W.view, 0), (W.shift, shiftMask)]]
+
+main = xmonad defaults
+
+defaults = defaultConfig
+    { terminal = myTerminal
+	, focusFollowsMouse = myFocusFollowsMouse
+    , modMask  = myModMask
+    , keys = myKeys
+    }
 
