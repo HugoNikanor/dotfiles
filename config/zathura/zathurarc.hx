4a5494f1868e04cbfe0ac31d5c811ecd46255137
Hugo Hörnquist <hugo.hornquist@gmail.com>
2017-04-20 21:46:01 +0200

    added zathura config

---
+++ config/zathura/zathurarc.hx
@@ -0,0 +1,14 @@
+set font "Iosevka Slab Medium 10"
+
+set statusbar-bg "#002b36" #04
+set statusbar-fg "#839496" #01
+
+set default-bg "#002b36" #00
+set default-fg "#839496" #01
+
+set recolor "true"
+set recolor-darkcolor  "#657b83"
+set recolor-lightcolor "#fdf6e3"
+
+set scroll-page-aware "true"
+
