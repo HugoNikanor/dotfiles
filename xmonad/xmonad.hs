da2af9304cedd61e20765ab7441b36f26f2c2780
Hugo Hörnquist <hugo.hornquist@gmail.com>
2017-09-07 19:54:03 +0200

    bunch of xmonad work

--- xmonad/xmonad.hs
+++ xmonad/xmonad.hs
@@ -1,42 +1,39 @@
-import XMonad
-import XMonad.Config.Desktop
-import XMonad.Util.SpawnOnce
+import System.IO (hPutStrLn)
+import Network.HostName (HostName, getHostName)
 
-import XMonad.Util.Types
+import XMonad
+
+import XMonad.Actions.PhysicalScreens (viewScreen, PhysicalScreen (..), sendToScreen)
+import XMonad.Actions.DynamicWorkspaces
+import XMonad.Actions.CycleWS (prevScreen, nextScreen, toggleWS)
+import XMonad.Actions.Warp (banish, Corner (..))
+import XMonad.Actions.WindowGo
+import XMonad.Actions.Navigation2D
+
+import XMonad.Hooks.DynamicLog
+import XMonad.Hooks.ManageDocks --(manageDocks, ToggleStruts)
+import XMonad.Hooks.InsertPosition
+
+import XMonad.Layout
+import XMonad.Layout.Grid (Grid (..))
+import XMonad.Layout.PerScreen
+import XMonad.Layout.SubLayouts
+import XMonad.Layout.WindowNavigation
+--import XMonad.Layout.Roledex
+import XMonad.Layout.Dishes (Dishes (..))
+import XMonad.Layout.IndependentScreens
+import XMonad.Layout.Maximize (maximize)
 
 import XMonad.Prompt
 
--- mod4mask?
+import XMonad.Util.SpawnOnce
+import XMonad.Util.Types
+import XMonad.Util.Run (spawnPipe)
 import XMonad.Util.EZConfig
 
 import qualified Data.Map as M
 import qualified XMonad.StackSet as W
-
-import XMonad.Layout
-import XMonad.Layout.Grid
-import XMonad.Layout.PerScreen
-
-import XMonad.Actions.PhysicalScreens
-
-import XMonad.Layout.SubLayouts
-import XMonad.Layout.WindowNavigation
 import qualified XMonad.Layout.BoringWindows as B
-
-import XMonad.Actions.DynamicWorkspaces
-import XMonad.Layout.IndependentScreens
-import XMonad.Actions.CycleWS
-
-import XMonad.Actions.Warp
-import XMonad.Actions.WindowGo
-import XMonad.Actions.Navigation2D
-import XMonad.Layout.Maximize
-
-import XMonad.Layout.Roledex
-import XMonad.Layout.Dishes
---import XMonad.Layout.Simplest
-
-import XMonad.Hooks.InsertPosition
-import Network.HostName
 
 -- xK_aring      = å
 -- xK_adiaeresis = ä
@@ -86,142 +83,162 @@ myKeys conf@(XConfig {XMonad.modMask = modm}) = M.fromList $
     , ((modm, xK_Tab ), windows W.focusDown)
     ]
 
+{-
+notSoSimlpeBindings :: [((KeyMask, KeySym), X ())]
+notSoSimlpeBindings =
+        [ ((modMask              , xK_odiaeresis), (viewScreen $ P 3) >> banish LowerRight)
+        , ((modMask .|. shiftMask, xK_odiaeresis), sendToScreen $ P 3)
+        ]
+        -}
+
+list :: a -> [a]
+list a = [a]
+
+xmobarCurrentWorkspaceColor = "#FFB6B0"
+xmobarTitleColor = "#CEFFAC"
+
+-- composeOne vs composeAll
+myManageHook = composeAll
+    [ className =? "Gvim" --> doFloat ]
+
 main = do
     termCommand <- getTerminalCommand <$> getHostName
     nScreens    <- countScreens
-    --addWorkspace "mail"
-    --withWorkspace def $ sendMessage . spawn "thunderbird"
-    xmonad $ def {
-          modMask            = mod4Mask
-        , focusFollowsMouse  = False
-        , clickJustFocuses   = False
-        , keys               = myKeys
-        , terminal           = termCommand
-        --, layoutHook         = subTabbed $ B.boringWindows $ ifWider tallThreshold wideLayouts tallLayouts
-        , layoutHook         = B.boringWindows $ maximize $ ifWider tallThreshold wideLayouts tallLayouts
-        , manageHook         = insertPosition Below Newer
-        -- , workspaces         = (withScreens nScreens $ show <$> [1..9]) ++ [mailWS]
-        , workspaces         = ["web", "irc", "mail", "term"]
-        , normalBorderColor  = "#1d1f21"
-        , focusedBorderColor = "#FF0000"
-        }
-        `additionalKeysP`
-        [ (pre "b", spawn gBrowser)
-        , (pre "e", spawn gEmacs)
-        --, (pre ++ "m", spawn gMail)
-        , (pre "y", spawn gRun)
-        , (pre "p", spawn gPass)
-        , (pre "i", spawn gIrc)
-        , (pre "s", spawn gSound)
-        , (pre "f", spawn gFiles)
+    xmproc      <- spawnPipe "xmobar ~/.xmonad/xmobar.hs"
+    xmonad $ def { modMask            = mod4Mask
+                 , focusFollowsMouse  = False
+                 , clickJustFocuses   = False
+                 , keys               = myKeys
+                 , terminal           = termCommand
+                 --, layoutHook         = subTabbed $ B.boringWindows $ ifWider tallThreshold wideLayouts tallLayouts
+                 , layoutHook         = B.boringWindows $ maximize $ ifWider tallThreshold wideLayouts tallLayouts
+                 , manageHook         = manageDocks <+> myManageHook <+> insertPosition Below Newer
+                 , workspaces         = ["web", "irc", "mail", "term"]
+                 , normalBorderColor  = "#1d1f21"
+                 , focusedBorderColor = "#FF0000"
+                 , logHook            = dynamicLogWithPP $ xmobarPP
+                        { ppOutput  = hPutStrLn xmproc
+                        , ppTitle   = xmobarColor xmobarTitleColor "" . shorten 100
+                        , ppCurrent = xmobarColor xmobarCurrentWorkspaceColor ""
+                        , ppSep     = "    "
+                        }
+                 } `additionalKeysP`
+                 [ (pre 'b', spawn gBrowser)
+                 , (pre 'e', spawn gEmacs)
+                 , (pre 'y', spawn gRun)
+                 , (pre 'p', spawn gPass)
+                 , (pre 'i', spawn gIrc)
+                 , (pre 's', spawn gSound)
+                 , (pre 'f', spawn gFiles)
+                 , (pre 'n', spawn gQuickNote)
 
-        --, ("M-b", toggleOrView mailWS)
+                 , ("M-t", withFocused $ windows . W.sink)
 
-        , ("M-t", withFocused $ windows . W.sink)
+                 --, ("M-j", B.focusDown)
+                 --, ("M-k", B.focusUp)
+                 --, ("M-S-j", windows W.swapDown)
+                 --, ("M-S-k", windows W.swapUp)
 
-        --, ("M-j", B.focusDown)
-        --, ("M-k", B.focusUp)
-        --, ("M-S-j", windows W.swapDown)
-        --, ("M-S-k", windows W.swapUp)
+                 --, ("M-S-h", sendMessage Shrink)
+                 --, ("M-S-l", sendMessage Expand)
 
-        --, ("M-S-h", sendMessage Shrink)
-        --, ("M-S-l", sendMessage Expand)
+                 --, ("M-h", sendMessage $ IncMasterN    1)
+                 --, ("M-l", sendMessage $ IncMasterN $ -1)
 
-        --, ("M-h", sendMessage $ IncMasterN    1)
-        --, ("M-l", sendMessage $ IncMasterN $ -1)
+                 , ("M-o"  , (viewScreen $ P 0) >> banish LowerRight)
+                 , ("M-S-o", (sendToScreen $ P 0))
+                 , ("M-e"  , (viewScreen $ P 1) >> banish LowerRight)
+                 , ("M-S-e", (sendToScreen $ P 1))
+                 , ("M-u"  , (viewScreen $ P 2) >> banish LowerRight)
+                 , ("M-S-u", (sendToScreen $ P 2))
+                 -- , ("M-<oadiaeresis>", (viewScreen $ P 2) >> banish LowerRight)
+                 -- , ("M-S-<oadiaeresis>", (sendToScreen $ P 2))
 
-        , ("M-o", (viewScreen $ P 0) >> banish LowerRight)
-        , ("M-e", (viewScreen $ P 1) >> banish LowerRight)
-        , ("M-S-o", (sendToScreen $ P 0))
-        , ("M-S-e", (sendToScreen $ P 1))
+                 -- Can keybinds be dependent on current layout?
+                 , ("M-l", windowGo R False >> banish LowerRight)
+                 , ("M-h", windowGo L False >> banish LowerRight)
+                 , ("M-k", windowGo U False >> banish LowerRight)
+                 , ("M-j", windowGo D False >> banish LowerRight)
 
-        -- Can keybinds be dependent on current layout?
-        , ("M-l", windowGo R False >> banish LowerRight)
-        , ("M-h", windowGo L False >> banish LowerRight)
-        , ("M-k", windowGo U False >> banish LowerRight)
-        , ("M-j", windowGo D False >> banish LowerRight)
+                 , ("M-S-l", windowSwap R False)
+                 , ("M-S-h", windowSwap L False)
+                 , ("M-S-k", windowSwap U False)
+                 , ("M-S-j", windowSwap D False)
 
-        , ("M-S-l", windowSwap R False)
-        , ("M-S-h", windowSwap L False)
-        , ("M-S-k", windowSwap U False)
-        , ("M-S-j", windowSwap D False)
+                 , ("M-x", sendMessage $ ToggleStruts)
 
 
-        , ("M-s", toggleWS)
+                 , ("M-s", toggleWS)
 
-        -- Do I even want these?
-        -- especcially if they don't work on a per-screen basis
-        -- Possibly write some own which works together with IndependentScreens
-        , ("M-g"  , prevScreen)
-        , ("M-c"  , nextScreen)
-        --, ("M-S-g", shiftPrevScreen)
-        --, ("M-S-c", shiftNextScreen)
+                 -- Do I even want these?
+                 -- especcially if they don't work on a per-screen basis
+                 -- Possibly write some own which works together with IndependentScreens
+                 , ("M-g"  , prevScreen)
+                 , ("M-c"  , nextScreen)
+                 --, ("M-S-g", shiftPrevScreen)
+                 --, ("M-S-c", shiftNextScreen)
 
-        , ("M-C-k", withFocused $ sendMessage . mergeDir W.focusUp')
-        , ("M-C-j", withFocused $ sendMessage . mergeDir W.focusDown')
-        , ("M-C-z", withFocused $ sendMessage . MergeAll)
-        , ("M-C-v", withFocused $ sendMessage . UnMerge)
+                 , ("M-C-k", withFocused $ sendMessage . mergeDir W.focusUp')
+                 , ("M-C-j", withFocused $ sendMessage . mergeDir W.focusDown')
+                 , ("M-C-z", withFocused $ sendMessage . MergeAll)
+                 , ("M-C-v", withFocused $ sendMessage . UnMerge)
 
-        --, ("M-m", onGroup W.focusUp')
-        --, ("M-w", onGroup W.focusDown')
+                 --, ("M-m", onGroup W.focusUp')
+                 --, ("M-w", onGroup W.focusDown')
 
-        , ("M-y", spawn gRun)
+                 , ("M-y", spawn gRun)
 
-        , ("M-q", spawn xmonadRe)
-        , ("M-t", withFocused $ windows . W.sink)
+                 , ("M-q", spawn xmonadRe)
+                 , ("M-t", withFocused $ windows . W.sink)
 
-        , ("M-a", sendMessage $ IncMasterN 1)
-        , ("M-.", sendMessage $ IncMasterN (- 1))
+                 , ("M-a", sendMessage $ IncMasterN 1)
+                 , ("M-.", sendMessage $ IncMasterN (- 1))
 
-        , ("M-S-c", kill)
-        , ("M-m", sendMessage NextLayout) -- TODO get a better binding
+                 , ("M-S-c", kill)
+                 , ("M-m", sendMessage NextLayout) -- TODO get a better binding
 
-        , ("M-<Space> M-<Space>", selectWorkspace myXPConfig)
-        , ("M-<Space> <Space>", selectWorkspace myXPConfig)
+                 , ("M-<Space> M-<Space>", selectWorkspace myXPConfig)
+                 , ("M-<Space> <Space>", selectWorkspace myXPConfig)
 
-        , ("M-<Space> d", removeWorkspace)
-        , ("M-<Space> M-d", removeEmptyWorkspace)
-        , ("M-<Space> a", addWorkspacePrompt myXPConfig)
-        , ("M-<Space> s", withWorkspace myXPConfig (windows . W.shift))
-        -- This should also select that workspace
-        , ("M-<Space> S-s", withWorkspace myXPConfig (windows . W.shift))
+                 , ("M-<Space> d", removeWorkspace)
+                 , ("M-<Space> M-d", removeEmptyWorkspace)
+                 , ("M-<Space> a", addWorkspacePrompt myXPConfig)
+                 , ("M-<Space> s", withWorkspace myXPConfig (windows . W.shift))
+                 -- This should also select that workspace
+                 , ("M-<Space> S-s", withWorkspace myXPConfig (windows . W.shift))
 
-        --, ("M-u", selectWorkspace def)
-        --, ("M-i", spawnToWorkspace "mail" "xterm")
-        ] where pre      = ("M-f " ++)
+                 , ("M-<Space> r", renameWorkspace myXPConfig { autoComplete = Nothing })
 
-                gBrowser = "firefox"
-                gEmacs   = "emacsclient -c"
-                gMail    = "thunderbird"
-                gRun     = "dmenu_path | dmenu | $(which bash)"
-                gPass    = "passmenu"
-                gIrc     = "xterm -e ssh irc -t screen -x"
-                gSound   = "pavucontrol"
-                gFiles   = "thunar"
+                 --, ("M-u", selectWorkspace def)
+                 --, ("M-i", spawnToWorkspace "mail" "xterm")
+                 ] where pre = ("M-f " ++) . list
+                         gBrowser   = "firefox"
+                         gEmacs     = "emacsclient -c"
+                         gMail      = "thunderbird"
+                         gRun       = "dmenu_path | dmenu | $(which bash)"
+                         gPass      = "passmenu"
+                         gIrc       = "xterm -e ssh irc -t screen -x"
+                         gSound     = "pavucontrol"
+                         gFiles     = "thunar"
+                         gQuickNote = "gvim +'setlocal buftype=nofile' -n" --s <(echo -n i)"
 
-                xmonadRe = "xmonad --recompile; xmonad --restart"
-                mailWS   = "mail"
+                         xmonadRe = "xmonad --recompile; xmonad --restart"
 
-                myXPConfig = defaultXPConfig
-                    { position = Top -- CenteredAt
-                    , historySize = 1000
-                    , autoComplete = Just 1
-                    , font = myFont
-                    -- , searchPredicate = isInfixOf
-                    }
+                         myXPConfig = defaultXPConfig
+                             { position = Top -- CenteredAt
+                             , historySize = 1000
+                             , autoComplete = Just 1
+                             , font = myFont
+                             -- , searchPredicate = isInfixOf
+                             }
 
-                -- TODO possibly auto get this number dependend on the system
-                -- possibly also change to a check if a screen is higher than
-                -- it is wide, and then change layouts
-                tallThreshold = 1200
-                --wideLayouts   = Tall 1 (3/100) (3/5) ||| Full
-                wideLayouts = GridRatio (4/3) ||| Full
-                --tallLayouts = Grid ||| Full
-                tallLayouts = {- Roledex ||| -} Dishes 1 (1/4) ||| Full
-                --tallLayouts = subLayout [0, 1] $  $ Dishes 2 (1/6)
-                --tallLayouts = {- addTabs shrinkText -} subLayout [0, 1, 2, 3] (Simplest ||| Tall 1 0.2 0.5 ||| Circle) $ Tall 1 0.2 0.5 ||| Full
-                --tallLayouts = {- addTabs shrinkText -} subLayout [2] (Circle) $ Tall 1 0.2 0.5 ||| Full
-                ---tallLayouts = {- addTabs shrinkText -} subLayout [0,1]  (Roledex) $ Dishes 2 (1/6)  ||| Full
-                --
-                --tallLayouts = subLayout [0, 1] (Full ||| Grid )
+                         -- TODO possibly auto get this number dependend on the system
+                         -- possibly also change to a check if a screen is higher than
+                         -- it is wide, and then change layouts
+                         tallThreshold = 1200
+                         --wideLayouts   = Tall 1 (3/100) (3/5) ||| Full
+                         wideLayouts = GridRatio (4/3) ||| Full
+                         --tallLayouts = Grid ||| Full
+                         tallLayouts = {- Roledex ||| -} Dishes 1 (1/4) ||| Full
+
 
