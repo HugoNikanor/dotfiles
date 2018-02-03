9a0a6aa1ebc968a3e86229e3d70b8bc30e850394
Hugo Hörnquist <hugo@hornquist.se>
2018-02-03 13:51:33 +0100

    emacs stuff

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -40,6 +40,8 @@
 
 	paredit
 	geiser
+
+	haskell-mode
 	))
 
 (package-initialize)
@@ -65,16 +67,8 @@
 
 ;;; ------------------------------------------------------------
 
-;; (define-globalized-minor-mode
-;;   global-text-scale-mode
-;;   text-scale-mode
-;;   (lambda () (text-scale-mode 1))) 
-
-;;; Minor modes
 (evil-mode)
 (ivy-mode)
-;; (linum-mode)
-;; (global-linum-mode)
 (which-key-mode) ; Show possible next keys after some key presses 
 (show-paren-mode)
 (column-number-mode)
@@ -82,11 +76,12 @@
 (menu-bar-mode 0)
 (tool-bar-mode 0)
 (scroll-bar-mode -1)
-;; "Symbol's function definition is void: text-scale-mode"
-;;(text-scale-mode 1)
 (text-scale-set -1)
 
+(setq inhibit-startup-screen t)
+
 (define-key evil-normal-state-map "\C-u" 'evil-scroll-up)
+(define-key evil-motion-state-map "\C-u" 'evil-scroll-up)
 ;; (define-key evil-normal-state-map (string ?\n) 'evil-open-below)
 
 (defun prettify-scheme ()
@@ -98,50 +93,36 @@
 	  ("prod" . #x220f))))
 (add-hook 'scheme-mode-hook #'prettify-scheme)
 
-(defun extra-tex ()
-  ;; Can I hide \left \right?
+(defun prettify-tex ()
   ;; \mathnote{v = p} -> /v = p/ (but with larger slashes)
-  (setq prettify-symbols-alist
-	("\\pm" . 0)
-	("\\sqrt" . 0)))
 
+  ;; \par is currently replaced with something my emacs
+  ;; fails to render. I think it's a vertical tab.
+
+  (setq prettify-symbols-alist
+	(append
+	 prettify-symbols-alist
+	 '(("\\pm" . ?±)
+	   ("\\sqrt" . ?√)
+	   ("\\left(" . ?\()
+	   ("\\right)" . ?\))))))
+(add-hook 'tex-mode-hook #'prettify-tex)
 
 (global-prettify-symbols-mode 1)
 
-;; 'info-keys
-;; '(("l" info-last)
-;;   ("n" evil-search-next))
-
+(add-to-list 'Info-default-directory-list "/home/hugo/info")
 (defun info-binds ()
-  (print "entering info mode")
-  ;;(evil-define-key 'motion info-mode-map "l" 'info-last)
-  ;; this is activated when info mode is left...
-  (define-key evil-motion-state-map "l" 'info-last)
+  (evil-define-key 'motion Info-mode-map "l" 'Info-last)
+  ;; Find non-conflicting binding for this
+  ;; (evil-define-key 'motion Info-mode-map "n" 'evil-search-next)
   )
-;; is this for info reader of for texinfo editing
 (add-hook 'Info-mode-hook #'info-binds)
 
-(add-to-list 'Info-default-directory-list "/home/hugo/info")
-
-;; this doesn't work at all
-;; (define-key evil-motion-state-map "l" 'info-last)
-
-;;(define-key evil-motion-state-map "l" 'info-last)
-
-;; (info-mode-map
-;;  (?l info-last)
-;;  )
-
-;; (define-key evil-motion-state-map (kbd "SPC ;") 'paredit-comment-dwim)
-(define-key evil-visual-state-map (kbd "SPC ;") 'paredit-comment-dwim)
-;; evil-visual-state-local-map
 
 ;; (autoload 'enable-paredit-mode "paredit" "Turn on pseudo-structural editing of Lisp code." t)
 (defun paredit-stuff ()
-  ;; I think this doesn't work...
   (evil-define-key 'visual lisp-mode-map (kbd "SPC ;") 'paredit-comment-dwim)
   (enable-paredit-mode))
-
 
 (add-hook 'emacs-lisp-mode-hook       #'paredit-stuff)
 (add-hook 'eval-expression-minibuffer-setup-hook #'paredit-stuff)
@@ -154,12 +135,12 @@
 
 ;; geiser-repl-mode
 
-(setq inhibit-startup-screen t)
-
 ;; Geiser only looks at these, if this list is here 
 (setq geiser-active-implementations '(guile))
+;; geiser doesn't seem to find this file,
+;; and is thereby not able to write to it.
 (setq geiser-repl-history-filename
-      "/home/hugo/.emacs.d/geiser/history")
+      "~/.emacs.d/geiser/history")
 
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
@@ -168,7 +149,7 @@
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
    (quote
-    (evil-paredit geiser paredit xresources-theme which-key ivy evil-org evil))))
+    (haskell-mode evil-paredit geiser paredit xresources-theme which-key ivy evil-org evil))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
