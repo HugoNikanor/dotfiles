8f2ba6b0d6abd953dcf1e12b9cc3d52f92a8fc07
Hugo Hörnquist <hugo@hornquist.se>
2019-09-17 11:11:37 +0200

    Fix issue where mutt set's weren't rendered.

--- scripts/mutt.scm
+++ scripts/mutt.scm
@@ -15,10 +15,13 @@
         (format #t "set signature=~a~%" (get-field o '(mutt file account-signature)))
         (map-subtree o '(mutt my_hdr)
                      (lambda (name value)
-                       (format #t "my_hdr ~a: ~a" name value)))
+                       (format #t "my_hdr ~a: ~a~%" name value)))
         (map-subtree o '(mutt set)
                      (lambda (name value)
-                       (format #f "set ~a = ~a~%" name value)))))
+                       (format #t "set ~a = ~a~%" name
+                               (if (string-contains value " ")
+                                   (string-append "\"" value "\"")
+                                   value))))))
 
     (with-output-to-file (get-field o '(mutt file account-signature))
       (lambda () (display (get-field o '(mutt signature))) (newline)))
