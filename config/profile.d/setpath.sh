985ab829829416924212663ab92133a85c6e9cfa
Hugo Hörnquist <hugo@hornquist.se>
2019-12-18 16:01:40 +0100

    Clean up setpath.sh.

--- config/profile.d/setpath.sh
+++ config/profile.d/setpath.sh
@@ -1,25 +1,10 @@
-#
-# Helper script for setting environment variables, which should be set
-# globaly for my user.
-# Loaded by both .xsessionrc, as well as .bashrc.
-# Note that it must be sh compatible, to work with xsessionrc.
-#
+add () {
+	test -d "$1" && PATH="$1:$PATH"
+}
 
-# [ -z $HAVE_SET_PATH ] || return
+add ~/.local/bin
+add ~/bin
+add ~/.gem/ruby/*/bin
+add ~/.cabal/bin
 
-for path in \
-	"$HOME/.local/bin" \
-	"$HOME/bin" \
-	$HOME/.gem/ruby/*/bin \
-	"$HOME/.cabal/bin"
-do
-	if [ -d $path ]; then
-		PATH="$path:$PATH"
-	fi
-done
-
-export PATH
-
-export LD_LIBRARY_PATH="$HOME/.local/lib:$LD_LIBRARY_PATH"
-
-# export HAVE_SET_PATH=${PP:- 1}
+unset add
