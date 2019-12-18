4604d82751a55ce8687f6b592cc838e8048bed3d
Hugo Hörnquist <hugo@hornquist.se>
2019-12-18 14:10:13 +0100

    Fix mail.scm for non-lysator systems.

--- scripts/mail.scm
+++ scripts/mail.scm
@@ -224,9 +224,9 @@
 
 (account mutt-global-gpg (mutt-global)
          (set
-           (spoolfile
+           ;; (spoolfile "/var/mail/hugo")
            (crypt_use_gpgme yes)
-           (pgp_default_key "E376B3821453F4BE1ED6F3C1265514B158C4CA23"))))
+           (pgp_default_key "E376B3821453F4BE1ED6F3C1265514B158C4CA23")))
 
 
 
