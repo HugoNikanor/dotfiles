85e88dd5df3050e3c64bc2c18a0823e896fd9453
Hugo Hörnquist <hugo@hornquist.se>
2019-12-22 12:53:53 +0100

    Rewrote installer script.

--- installer.sh
+++ installer.sh
@@ -2,28 +2,34 @@
 # Items in config/ are symlinked as items in ~/.config/
 # other stuff is symlinked into ~/, but with a dot added beforehand
 
-function contains() {
-	list=$1
-	item=$2
-
-	echo $list | grep $item
+verbose () {
+	set -x
+	"$@"
+	set +x
 }
 
-set -x
-
-ignore_list="scripts,config,README.md,installer.sh"
-
-for file in $(ls); do
-	contains $ignore_list $file && continue
-
-	ln -s $(realpath $file) $HOME/.$file
+for file in $(ls \
+	-I config \
+	-I dosbox* \
+	-I installer.sh \
+	-I README.md \
+	-I mutt \
+	-I elinks
+	-I scripts)
+do
+	[ -h $HOME/.$file ] || verbose ln -s $(realpath $file) $HOME/.$file
 done
-
-pushd
 
 dir=$(realpath config)
-for file in $(ls config/); do
-	cd $HOME/.config/ && ln -s $dir/$file
+pushd $HOME/.config/
+for file in $(ls $dir); do
+	[ -h $file ] || verbose ln -s $dir/$file
 done
-
 popd
+
+pushd scripts
+for file in $(ls); do
+	[ -x $file ] && verbose ./$file
+done
+popd
+
