bc7e5b1e50a0c2995f148ff2914ce7ae3b95a2c9
Hugo Hörnquist <hugo@hornquist.se>
2019-12-30 04:25:50 +0100

    Future ideas for log-view-mode.

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -558,10 +558,31 @@ STR: target string"
         (,(regexp-opt '("fail")) . 'error))
       )
 
+  ;; M-x describe-face
 (define-derived-mode log-mode fundamental-mode "Log"
   "Major mode for viewing srfi-64 log files from guile."
 
-  ;; M-x describe-face
+  ;; fold cases
+;; Test begin:
+;;   source-file: "/home/hugo/code/calparse/tests/termios.scm"
+;;   source-line: 34
+;;   source-form: (test-equal (& ifl (~ (|| ECHO ICANON))) (lflag t))
+;; Test end:
+;;   result-kind: pass
+;;   actual-value: 0
+;;   expected-value: 0
+  ;; folds to
+  ;; PASS <name if name> <source-file : source-line> ...
+
+  ;; success ration pinned to top of screen
+  ;; parsed from end of file
+;; # of expected passes      22
+;; # of unexpected failures  4
+
+  ;; Failing values should highlight their difference
+
+  ;; Groups should be marked/indented?
+
 
   (read-only-mode 1)
   (setq font-lock-defaults '(log-mode-highlights))
