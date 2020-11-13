05c561ad9ce693660c8396b6f3c2e8123d7524fc
Hugo Hörnquist <hugo@lysator.liu.se>
2020-11-13 21:57:12 +0100

    [Bash] Update nmail alias to match updated newmail.

--- config/bash/bash-aliases.sh
+++ config/bash/bash-aliases.sh
@@ -14,7 +14,7 @@ alias wi="wiki"
 alias wip="wi -w public"
 alias wig="wi -w private"
 alias net="watch -n1 -c nmcli -c yes"
-alias nmail="watch -n1 -c newmail"
+alias nmail="newmail --watch"
 
 alias weeslack="weechat -d ~/.local/share/weechat/slack"
 
