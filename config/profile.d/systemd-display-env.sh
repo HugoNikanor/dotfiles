2af9c119c82220eab12f69190224f4c86e2bee22
Hugo Hörnquist <hugo@lysator.liu.se>
2022-09-21 00:08:47 +0200

    Move setup and start of xcape to xinitrc.

--- config/profile.d/systemd-display-env.sh
+++
@@ -1,3 +0,0 @@
-[ -z "$DISPLAY" ] && return
-
-systemctl import-environment --user DISPLAY
--- config/profile.d/xinit-xcape.sh
+++
@@ -1,6 +0,0 @@
-[ -z "$DISPLAY" ] && return
-[ -n "$SSH_CLIENT" ] && return
-
-command -v xcape > /dev/null || return
-
-xcape -e "Shift_L=parenleft;Shift_R=parenright"
